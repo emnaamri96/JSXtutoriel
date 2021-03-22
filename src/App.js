62ba9166147c0e77553376c3381ad58b464326f1
emnaamri96 <emna.amri8@gmail.com>
2021-03-22 11:44:56 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import image from './images/image1.jpg';
+import style from './style.css';
+
+
+
+function App()
+ {
+    return (
+     <div className='menu'>
+        <h1 className='title'> Disney Cartoons</h1> 
+          < img src ="./pictures/image2.jpg"alt=""/>
+           <img clasName="lionpsic" src ="./images/image1.jpg"alt=""/>
+             <video className="disneyintro" width='320' height="240" controls/>
+               <source src='https://www.youtube.com/watch?v=tmndjvzfNA8'/>
+  
+   
+   
+        
+
+
+      </div>
+  );
+}
+
+export default App;
+
